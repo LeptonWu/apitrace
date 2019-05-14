f7e991e19432af2b1d4a0c69e39fcae46acff0ad
Lepton Wu <ytht.net@gmail.com>
2019-05-14 12:16:49 -0700

    Also use RTLD_NEXT to resolve dlopen on Android.
    RTLD_NEXT works on recent versions of Android. If it fail, we fall back to the
    old RTLD_DEFAULT way.

--- dispatch/dlopen.hpp
+++ dispatch/dlopen.hpp
@@ -44,14 +44,14 @@ _dlopen(const char *filename, int flag)
     static PFN_DLOPEN dlopen_ptr = NULL;
 
     if (!dlopen_ptr) {
+        dlopen_ptr = (PFN_DLOPEN)dlsym(RTLD_NEXT, "dlopen");
 #ifdef ANDROID
-        /* Android does not have dlopen in libdl.so; instead, the
+        /* Legacy Android does not have dlopen in libdl.so; instead, the
          * implementation is available in the dynamic linker itself.
          * Oddly enough, we still can interpose it, but need to use
          * RTLD_DEFAULT to get pointer to the original function.  */
-        dlopen_ptr = (PFN_DLOPEN)dlsym(RTLD_DEFAULT, "dlopen");
-#else
-        dlopen_ptr = (PFN_DLOPEN)dlsym(RTLD_NEXT, "dlopen");
+        if (!dlopen_ptr)
+            dlopen_ptr = (PFN_DLOPEN)dlsym(RTLD_DEFAULT, "dlopen");
 #endif
         if (!dlopen_ptr) {
             os::log("apitrace: error: failed to look up real dlopen\n");
